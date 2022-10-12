a533a9c3e7bef9fe27d21d1a8de9f1da23b02ad3
Alvi <alviand03@gmail.com>
2022-10-12 16:33:14 +0700

    add bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => const SplashPage(),
+        '/': (context) => const HomePage(),
         '/onboarding': (context) => const OnboardingPage(),
         '/sign-in': (context) => const SignInPage(),
         '/sign-up': (context) => const SignUpPage(),
